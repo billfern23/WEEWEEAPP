5cb5245818a3d495a8b04da308e644870d4adfc0
billfern23 <billfern23@gmail.com>
2021-12-10 14:06:42 -0500

    changes done

--- src/app/lotterynumbers/lotterynumbers.component.ts
+++ src/app/lotterynumbers/lotterynumbers.component.ts
@@ -14,6 +14,7 @@ export class LotterynumbersComponent implements OnInit {
   selected:number[] = []
   s:number = 1
   flagClear:boolean = true
+   temp:number[] = []
   constructor() { }
 
    ngOnInit(){
@@ -27,31 +28,60 @@ export class LotterynumbersComponent implements OnInit {
   }
   @Output() pickedNumber = new EventEmitter<number[]> ();
   @Output() flagTotal = new EventEmitter<boolean> ();
+
+  /*
+  Only function that matter, if length < 5 keep changing the flag from true to false
+  to change the color of the button, 
+  if false and length of numbers array < 5: just keep pushing it into the numbers array
+  if false: then use filter to create a new array that is not equal to the value
+  tricky part: if length = 5, you dont want to let any more buttons to be clicked
+  so you do the reverse of it, filter for those numbers that are only present in the numbers array
+  and then change the flag 
+  Boom render it.
+  Since this is in the child you want to pass the changes to any number up to the parent thats where my
+  emit comes in, only required once, flagClear takee care of both cash and clear cause they become active in simulatanousely.
+  */
   clicker(n:{value:number, flag:Boolean}){
-
-    if(this.selected.length < 5 && n.flag === false){
-
-      n.flag = true;
-      this.selected.push(n.value)
-
-      const pickedNumber = this.selected
-      this.pickedNumber.emit(pickedNumber)
+    if(this.selected.length <5){
+      
+      n.flag = (!n.flag)
+    
     }
-    if(this.selected.length === 5){
+
+    if(this.selected.length ==4 ){
       this.flagClear = false;
       const flagTotal = false
-    this.flagTotal.emit(flagTotal)
-  
+      this.flagTotal.emit(flagTotal)
     }
- 
-     //this.flagButton = (!this.flagButton)
-     //window.alert(n)
+  
+    if(this.selected.length === 5){
+      
+      this.temp = this.selected.filter(el => el == n.value)
+      if(this.temp){
+        if(n.value == this.temp[0]){
+          n.flag = (!n.flag)
+          this.flagClear = true;
+        }
+
+      }
+     
+    }
+
+    if(n.flag){
+      if(this.selected.length <5){
+        this.selected.push(n.value)
+      
+      }
+    }
+    else {
+      this.temp = this.selected.filter(el => el != n.value)
+      this.selected = this.temp;
+    }
+    const pickedNumber = this.selected
+    this.pickedNumber.emit(pickedNumber)
+    
    }
  
-   cashClick()
-   {
-     console.log("Hello")
-   }
 
    clear(){
     this.buttonsArray=[]
